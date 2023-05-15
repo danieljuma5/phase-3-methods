8403ef929672fdf8b1bbe23b7361bf9c4bfabf9f
danieljuma5 <jumadaniel275@gmail.com>
2023-05-15 14:32:28 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
-# Your code here!
+# Writing methods in ruby
+
+def greet_programmer
+    puts "Hello, programmer!"
+ end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer" )
+  puts "Hello, #{name}!"
+end
+
+def add(num1,num2)
+  return num1 + num2
+end
+
+def halve(num)
+  if num.class == Integer
+    return num / 2
+  end
+  return nil
+  end
